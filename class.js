0e4d73c9b50205595d85f8db5ae5e8c597dc4514
nayem9b <razwanniam1@gmail.com>
2023-07-06 11:00:14 +0600

    class of js

---
+++ class.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name, email) {
+    console.log(name, email);
+  }
+} //Here we are creating a class
+
+const p1 = new Person("typed", "based"); // Here we are creating a new instance. Like the template of the class
+p1.name = " Nayem";
+p1.email = "email";
+
+// Here Person() is a constructor function
