88d0b07a195a7ce2c1a63cd22458d223c41c1801
nayem9b <razwanniam1@gmail.com>
2023-09-25 23:23:26 +0600

    project modified

--- class.js
+++ class.js
@@ -3,9 +3,9 @@ class Person {
     this.name = name;
     this.email = email;
   }
-} //Here we are creating a class
+// } //Here we are creating a class
 
-const p1 = new Person("typed", "based"); // Here we are creating a new instance. Like the template of the class
+// const p1 = new Person("typed", "based"); // Here we are creating a new instance. Like the template of the class
 const p2 = new Person("Javascript", "Typescript");
 
 // Here we are passing the data through creating an instance and the Person is a class which holds the properties
